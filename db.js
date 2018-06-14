fe8f5885880d50b57fd0622678a544f1fae21570
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-14 20:26:27 +1200

    added two routes

--- db.js
+++ db.js
@@ -3,10 +3,18 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  getProperties
+  getProperties,
+  getProperty
 }
 
 function getProperties (conn = connection) {
   return conn('properties')
     .select()
+}
+
+function getProperty (id, conn = connection) {
+  return conn('properties')
+    .join('feedback', 'properties.id', 'feedback.property_id')
+    .select()
+    .where('properties.id', id)
 }
