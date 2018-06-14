e90cf5d7783ec5304b12052ca404e0f2e00da662
User <user@Users-MacBook-Pro.local>
2018-06-15 10:49:28 +1200

    home page route alg

---
+++ test/testingRoutes.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+const cheerio = require('cheerio')
+
+const server = require('../server')
+
+// const testEnv = require('./test-environment')
+
+// beforeEach(() => {
+//   const testDb = testEnv.getTestDb()
+//   return testEnv.initialise(testDb)
+// })
+
+// afterEach(() => testEnv.cleanup(testDb))
+
+test('home page ', (done) => {
+  return request(server)
+    .get('/')
+    .expect(200)
+    .end((err, res) => {
+      // console.log('res-text: ', res.text)
+      const $ = cheerio.load(res.text)
+      const paragraph = $('p').text()
+      expect(paragraph).toMatch('home')
+      done(err)
+    })
+})
