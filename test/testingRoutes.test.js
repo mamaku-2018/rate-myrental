fe70802156b5b8062e0e0853f56dceff6d27e701
User <user@Users-MacBook-Pro.local>
2018-06-15 15:24:44 +1200

    check

--- test/testingRoutes.test.js
+++ test/testingRoutes.test.js
@@ -1,16 +1,17 @@
+const db = require('../db')
 const request = require('supertest')
 const cheerio = require('cheerio')
-
 const server = require('../server')
+const testEnv = require('./test-environment')
 
-// const testEnv = require('./test-environment')
+let testDb = null
 
-// beforeEach(() => {
-//   const testDb = testEnv.getTestDb()
-//   return testEnv.initialise(testDb)
-// })
+beforeEach(() => {
+  testDb = testEnv.getTestDb()
+  return testEnv.initialise(testDb)
+})
 
-// afterEach(() => testEnv.cleanup(testDb))
+afterEach(() => testEnv.cleanup(testDb))
 
 test('home page ', (done) => {
   return request(server)
@@ -25,15 +26,18 @@ test('home page ', (done) => {
     })
 })
 
-test('properties page ', (done) => {
-  return request(server)
-    .get('/properties')
-    .expect(200)
-    .end((err, res) => {
-      // console.log('res-text: ', res.text)
-      const $ = cheerio.load(res.text)
-      const paragraph = $('h1').text()
-      expect(paragraph).toMatch('PROPERTIES')
-      done(err)
+test('properties page has 3 properties', () => {
+  return db.getProperties(testDb)
+    .then(properties => {
+      expect(properties.length).toBe(3)
     })
 })
+
+// test('test property has 1 feedback', () => {
+//   const id = 2
+//   return db.getPropertyFeedback(id, testDb)
+//     .then(property => {
+//       expect(property.length).toBe(1)
+//       expect(property[0].type).toBe('Test Sedan')
+//     })
+// })
